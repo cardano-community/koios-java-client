9ecf1927e7f82b6bddf25de7002de9489d9903e4
Dudi Edri <edridudi@gmail.com>
2023-01-22 11:41:20 +0200

    Cosmetics

--- src/main/java/rest/koios/client/utils/Bech32Util.java
+++ src/main/java/rest/koios/client/utils/Bech32Util.java
@@ -7,14 +7,18 @@ import org.bouncycastle.util.Arrays;
  */
 public class Bech32Util {
 
-    private static final int TotalMaxLength = 108; //103 = mainnet length of a delegation address, 108 = testnet length
-    private static final int CheckSumSize = 6;
-    private static final int HrpMinLength = 1;
-    private static final int HrpMaxLength = 83;
-    private static final int HrpMinValue = 33;
-    private static final int HrpMaxValue = 126;
-    private static final char Separator = '1';
-    private static final String B32Chars = "qpzry9x8gf2tvdw0s3jn54khce6mua7l";
+    private Bech32Util() {
+        throw new IllegalStateException("Utility Class");
+    }
+
+    private static final int TOTAL_MAX_LENGTH = 108; //103 = mainnet length of a delegation address, 108 = testnet length
+    private static final int CHECK_SUM_SIZE = 6;
+    private static final int HRP_MIN_LENGTH = 1;
+    private static final int HRP_MAX_LENGTH = 83;
+    private static final int HRP_MIN_VALUE = 33;
+    private static final int HRP_MAX_VALUE = 126;
+    private static final char SEPARATOR = '1';
+    private static final String B_32_CHARS = "qpzry9x8gf2tvdw0s3jn54khce6mua7l";
 
     /**
      * isValid
@@ -28,7 +32,7 @@ public class Bech32Util {
             return false;
         }
         Tuple<String, byte[]> data = bech32Decode(bech32EncodedString);
-        if (data._2.length < CheckSumSize) {
+        if (data._2.length < CHECK_SUM_SIZE) {
             return false;
         }
 
@@ -43,7 +47,7 @@ public class Bech32Util {
      * @return true whether bech32 Encoded String contains valid Chars, false otherwise
      */
     public static boolean hasValidChars(String bech32EncodedString) {
-        if ((bech32EncodedString == null || bech32EncodedString.isEmpty()) || bech32EncodedString.length() > TotalMaxLength) {
+        if ((bech32EncodedString == null || bech32EncodedString.isEmpty()) || bech32EncodedString.length() > TOTAL_MAX_LENGTH) {
             return false;
         }
 
@@ -53,7 +57,7 @@ public class Bech32Util {
         }
 
         // Check if it has a separator
-        int sepIndex = bech32EncodedString.lastIndexOf(Separator);
+        int sepIndex = bech32EncodedString.lastIndexOf(SEPARATOR);
         if (sepIndex == -1) {
             return false;
         }
@@ -66,15 +70,15 @@ public class Bech32Util {
 
         // Validate data part
         String data = bech32EncodedString.substring(sepIndex + 1);
-        return data.length() >= CheckSumSize && data.chars().noneMatch(x -> B32Chars.indexOf(x) == -1);
+        return data.length() >= CHECK_SUM_SIZE && data.chars().noneMatch(x -> B_32_CHARS.indexOf(x) == -1);
     }
 
     private static boolean isValidHrp(String hrp) {
         return hrp != null &&
                 hrp.trim().length() > 0 &&
-                hrp.length() >= HrpMinLength &&
-                hrp.length() < HrpMaxLength &&
-                hrp.chars().allMatch(character -> character >= HrpMinValue && character <= HrpMaxValue);
+                hrp.length() >= HRP_MIN_LENGTH &&
+                hrp.length() < HRP_MAX_LENGTH &&
+                hrp.chars().allMatch(character -> character >= HRP_MIN_VALUE && character <= HRP_MAX_VALUE);
     }
 
     private static int polymod(final byte[] values) {
@@ -94,8 +98,8 @@ public class Bech32Util {
     private static byte[] expandHrp(String hrp) {
         byte[] result = new byte[(2 * hrp.length()) + 1];
         for (int i = 0; i < hrp.length(); i++) {
-            result[i] = (byte) (((int) hrp.charAt(i)) >> 5);
-            result[i + hrp.length() + 1] = (byte) (((int) hrp.charAt(i)) & 0b0001_1111 /*=31*/);
+            result[i] = (byte) (( hrp.charAt(i)) >> 5);
+            result[i + hrp.length() + 1] = (byte) (( hrp.charAt(i)) & 0b0001_1111 /*=31*/);
         }
         return result;
     }
@@ -109,15 +113,15 @@ public class Bech32Util {
 
         bech32EncodedString = bech32EncodedString.toLowerCase();
 
-        int separatorIndex = bech32EncodedString.lastIndexOf(Separator);
+        int separatorIndex = bech32EncodedString.lastIndexOf(SEPARATOR);
         String hrp = bech32EncodedString.substring(0, separatorIndex);
         String data = bech32EncodedString.substring(separatorIndex + 1);
 
         byte[] b32Arr = new byte[data.length()];
         for (int i = 0; i < data.length(); i++) {
-            b32Arr[i] = (byte) B32Chars.indexOf(data.charAt(i));
+            b32Arr[i] = (byte) B_32_CHARS.indexOf(data.charAt(i));
         }
 
-        return new Tuple(hrp, b32Arr);
+        return new Tuple<>(hrp, b32Arr);
     }
 }
